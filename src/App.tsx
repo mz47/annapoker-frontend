af8ab62c01ba8f2df5fcba5768dcb9a509b843f3
Marcel Prehn <marcel.prehn@aclue.de>
2020-08-24 20:05:11 +0200

    Add ImpressumPage.tsx Add BuyMeACoffee Link

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {Route} from "react-router-dom";
 import PokerPage from "./pages/PokerPage";
 import {StartPage} from "./pages/StartPage";
 import {NewSessionPage} from "./pages/NewSessionPage";
+import {ImpressumPage} from "./pages/ImpressumPage";
 
 const App = () => (
   <Box>
     <Route path={"/"} exact component={StartPage}/>
     <Route path={"/new"} component={NewSessionPage}/>
     <Route path={"/poker/:sessionId"} component={PokerPage}/>
+    <Route path={"/impressum"} component={ImpressumPage}/>
   </Box>
 )
 
--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import {Box, Button, Header, Heading} from "grommet/es6";
-import {AddCircle, Home, Login, Refresh, View} from "grommet-icons/es6";
+import {Box, Button, Header, Heading, Text} from "grommet/es6";
+import {AddCircle, Github, Home, Login, Refresh, View} from "grommet-icons/es6";
 import {Link} from "react-router-dom";
 
 interface NavHeaderProps {
   loginHandler?: () => void,
   revealHandler?: () => void,
   resetVotingsHandler?: () => void,
-  loginDisabled: boolean,
-  revealDisabled: boolean,
-  resetVotingsDisabled: boolean,
-  resetUsersDisabled: boolean,
+  loginDisabled?: boolean,
+  revealDisabled?: boolean,
+  resetVotingsDisabled?: boolean,
+  resetUsersDisabled?: boolean,
 }
 
 export const NavHeader = (props: NavHeaderProps) => {
   const {loginHandler, resetVotingsHandler, revealHandler, revealDisabled, loginDisabled} = props
   return (
     <Header background={"brand"} justify={"start"}>
-      <Heading margin={"small"}>Annapoker</Heading>
-      <Link to={"/"}><Button icon={<Home/>} hoverIndicator/></Link>
+      <Link to={"/"}><Heading margin={"small"}>Annapoker</Heading></Link>
       <Link to={"/new"}><Button icon={<AddCircle/>} hoverIndicator/></Link>
-      <Button icon={<Login/>} disabled={loginDisabled} onClick={loginHandler} hoverIndicator/>
+      <Button icon={<Login/>} disabled={loginDisabled || true} onClick={loginHandler} hoverIndicator/>
       <Box direction={"row"} align={"center"}>
         <span>|</span>
-        <Button icon={<View/>} disabled={revealDisabled} onClick={revealHandler}
+        <Button icon={<View/>} disabled={revealDisabled || true} onClick={revealHandler}
                 hoverIndicator/>
-        <Button icon={<Refresh/>} disabled={revealDisabled} onClick={resetVotingsHandler} hoverIndicator/>
+        <Button icon={<Refresh/>} disabled={revealDisabled || true} onClick={resetVotingsHandler} hoverIndicator/>
+      </Box>
+      <Box fill={true} direction={"row"} align={"center"}>
+        <Box flex={"grow"} direction={"column"} margin={"medium"}>
+          <Text textAlign={"end"}><Link to={"/impressum"}>Impressum</Link></Text>
+        </Box>
+        <Box direction={"column"} margin={"small"}>
+          <a href="https://www.buymeacoffee.com/MarcelPrehn" target="_new">
+            <img src="https://cdn.buymeacoffee.com/buttons/default-white.png" alt="Buy Me A Coffee" className={"coffee"}/>
+          </a>
+        </Box>
       </Box>
     </Header>
+
   )
 }
