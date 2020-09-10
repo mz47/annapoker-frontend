f1fc06310af5c6bf4f01b225285b5c034e514eaf
Marcel Prehn <marcel.prehn@aclue.de>
2020-09-10 20:38:31 +0200

    Add intro to start page

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import {Box, Heading, Image, Main} from "grommet/es6";
+import {Box, Button, Heading, Main} from "grommet/es6";
 import {NavHeader} from "../components/NavHeader";
+import {Text} from "grommet";
+import {Login, Logout, Refresh, View} from "grommet-icons";
+import {AddCircle} from "grommet-icons/es6";
+import {Link} from "react-router-dom";
 
 export const StartPage = () => {
   return (
@@ -8,10 +12,24 @@ export const StartPage = () => {
       <NavHeader loginDisabled={true} revealDisabled={true} resetVotingsDisabled={true} resetUsersDisabled={true}
                  logoutDisabled={true}/>
       <Main pad="large" align={"center"}>
-        <Box>
+        <Box align={"center"}>
           <Heading>Welcome to Annapoker!</Heading>
-          <Image fit={"contain"} src={"https://media.giphy.com/media/fat0moDHhsjO8/source.gif"}
-                 alt={"Terence Hill Poker Giphy"}/>
+          <Box direction={"row"} align={"center"}>
+            <Text margin={"medium"} size={"xxlarge"} className={"intro"}>
+              Click <Button primary
+                            size={"small"}
+                            icon={<AddCircle/>}
+                            margin={"xxsmall"}/> to generate a new Annapoker session.<br/>
+              After that, open the new session and share the link.<br/><br/>
+              Following actions are possible during your Annapoker session<br/>
+              <ul className="actions-list">
+                <li><Login/><Text margin={"small"} size={"xlarge"}>Login again</Text></li>
+                <li><Logout/><Text margin={"small"} size={"xlarge"}>Leave session</Text></li>
+                <li><View/><Text margin={"small"} size={"xlarge"}>Reveal all votings for everyone</Text></li>
+                <li><Refresh/><Text margin={"small"} size={"xlarge"}>Reset current votings</Text></li>
+              </ul>
+            </Text>
+          </Box>
         </Box>
       </Main>
     </Box>
